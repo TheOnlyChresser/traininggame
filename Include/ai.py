2de2b66f87c2ab142c1b08c5be25d5fa9aa4b523
ZakersLaptop <nukl3430@gmail.com>
2026-02-03 20:05:26 +0100

    commit

--- Include/ai.py
+++ Include/ai.py
@@ -1,8 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR  ### temp 
+import math
+
+from data.torchDATA import TorcHdata
+
 class dataStruct:
     def __init__(self):
 #       validate input
-        self.lossFunktioner = []
-        self.aktiveringsFunktion = []
         self.dataset = []
         self.datafile = ""
 #       load from json file
@@ -16,20 +23,23 @@ class dataStruct:
 
 
 class UserAI:
-    def __init__(self, Lag, Bias, Vægte, AktiveringsFunktion, LossFunktion, Dataset):
+    def __init__(self, Lag, Bias, Vægte, AktiveringsFunktion, LossFunktion, Dataset, device, Epocs, LearningRate=1e-6):
    
 #        if not ValidInput:
 #            exit
-
         self.Lag = Lag
         self.Bias = Bias
         self.Vægte = Vægte
+        self.LearningRate = LearningRate
         self.AktiveringsFunktion = AktiveringsFunktion
         self.LossFunktion = LossFunktion
         self.Dataset = Dataset
+        self.Epocs = Epocs
+        self.modelInfo = {}
+        self.device = device
     
     def Setter(self, variableSet):
-        exit
+        pass
 
     def Getter(self, variableGet):
         exit
@@ -41,4 +51,17 @@ class UserAI:
         exit
     
     def NNconstructor(self):
-        exit
+        for i in range(len(self.Lag)):
+            self.modelInfo.update({self.Lag[i] + i:TorcHdata["lagTyper"][self.Lag]})
+
+    def Run(self):
+        w = torch.empty((max(self.Lag), len(self.Lag)), dtype=torch.float64)
+        
+        torch.nn.init.xavier_uniform_(w, gain=torch.nn.init.calculate_gain("softmax"))
+        x = torch.linspace(-math.pi, math.pi, self.Lag[0], dtype=torch.float64)
+        y = torch.sin(x)
+
+        model = nn.Sequential(self.modelInfo)
+
+        for i in range(self.Epocs):
+            y_pred = model()
--- data/torchDATA.py
+++ data/torchDATA.py
@@ -1,9 +1,48 @@
-﻿import torch.nn as nn
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR  ### temp 
+import math
 
 TorcHdata = {
-    "lagTyper": {
-        "convolutionLayer": nn.Conv2d,
-        "max": max,
+    "lagTyper":{
+        "Conv2d":nn.Conv2d,
+        "Transformer":nn.Transformer,
+        "Dropout":nn.Dropout,
+        "Linear":nn.Linear,
+        "RNN":nn.RNN,
+        "GRU":nn.GRU,
+        "LSTMCell":nn.LSTMCell,
+        "SyncBatchNorm":nn.SyncBatchNorm,
+        "RMSNorm":nn.RMSNorm,
+        "CircularPad1d":nn.CircularPad1d,
+        "ConstantPad1d":nn.ConstantPad1d,
+        "ZeroPad1d":nn.ZeroPad1d,
+        "MaxPool1d":nn.MaxPool1d,
+        "AvgPool1d":nn.AvgPool1d,
+        "FractionalMaxPool2d":nn.FractionalMaxPool2d,
+        "AdaptiveMaxPool1d":nn.AdaptiveMaxPool1d,
+        "LazyConv2d":nn.LazyConv2d,
+        "ConvTranspose2d":nn.ConvTranspose2d,
+        "LazyConvTranspose2d":nn.LazyConvTranspose2d
+    },
+    "lossFunktioner":{
+        "L1Loss":nn.L1Loss,
+        "MSELoss":nn.MSELoss,
+        "GaussianNLLLoss":nn.GaussianNLLLoss,
+        "HuberLoss":nn.HuberLoss,
+        "CosineEmbeddingLoss":nn.CosineEmbeddingLoss,
+        "BCEWithLogitsLoss":nn.BCEWithLogitsLoss,
+        "CTCLoss":nn.CTCLoss,
+        "KLDivLoss":nn.KLDivLoss,
+    },
+    "aktFunktioner":{
+        "ELU":nn.ELU,
+        "LeakyReLU":nn.LeakyReLU,
+        "LogSigmoid":nn.LogSigmoid,
+        "ReLU":nn.ReLU,
+        "Softmax":nn.Softmax,
+        "Tanh":nn.Tanh,
     }
 }
